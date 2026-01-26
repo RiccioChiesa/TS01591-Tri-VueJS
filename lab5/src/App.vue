1141ee47ffe7197a842abf5291b43a93e3fefe39
Minh Tri <trinhmts01591@gmail.com>
2026-01-26 19:42:19 +0700

    cau truc app vue ca bai

--- lab5/src/App.vue
+++ lab5/src/App.vue
@@ -1,19 +1,28 @@
 <script setup>
 
 import Bai1 from './components/Bai1.vue';
+import Bai2 from './components/Bai2.vue';
+import Bai3 from './components/Bai3.vue';
+import Bai4 from './components/Bai4.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
+  </div> -->
 
-    <Bai1 />
+    <!-- <Bai1 />
+    <hr />
+    <Bai2 /> -->
+      <!-- <hr />
+  <Bai3 /> -->
+   <hr />
+      <Bai4 />
 </template>
 
 <style scoped>
